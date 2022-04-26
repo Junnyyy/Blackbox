ecb6da28b5662165e9c5fa4a1bab1e6102f2f264
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-04-26 01:15:36 -0500

    Update ping command to return proper ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,18 +5,39 @@ import { Command } from "../interfaces/Command";
 export const ping: Command = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Ping command"),
+    .setDescription("Transmits Websocket latency and ping."),
   run: async (interaction) => {
     await interaction.deferReply();
     const { user } = interaction;
 
-    const returnMessage = new MessageEmbed();
-    returnMessage.setTitle("Ping!");
-    returnMessage.setDescription("Pong!");
-    returnMessage.setAuthor({
-      name: user.tag,
-      iconURL: user.displayAvatarURL(),
-    });
+    let output: string = `**Gateway:** Online ✅`;
+    output += `\n**Websocket Latency:** ${interaction.client.ws.ping} ms`;
+    output += `\n**API Latency:** ${
+      Date.now() - interaction.createdTimestamp
+    } ms`;
+
+    // Alternative Embeded (Triggers typescript errors)
+    // const returnMessage = {
+    //   color: "ffeded",
+    //   title: `**Pong** 🏓`,
+    //   description: output,
+    //   timestamp: new Date(),
+    //   footer: {
+    //     text: user.tag,
+    //     icon_url: user.displayAvatarURL(),
+    //   },
+    // };
+
+    const returnMessage = new MessageEmbed()
+      .setColor("#ffeded")
+      .setTitle(`**Pong** 🏓`)
+      .setDescription(output)
+      .setTimestamp()
+      .setFooter(`${user.tag}`, `${user.avatarURL({ format: "png" })}`);
+    // returnMessage.setAuthor({
+    //   name: user.tag,
+    //   iconURL: user.displayAvatarURL(),
+    // });
 
     await interaction.editReply({ embeds: [returnMessage] });
   },
