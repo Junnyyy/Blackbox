0a772dd787e43ded496dd8cc953804477e3dcbfe
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-05-06 01:11:15 -0500

    updated validate env

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -9,5 +9,30 @@ export const validateEnv = () => {
     return false;
   }
 
+  if (!process.env.CREATORID) {
+    console.warn("Missing Creator ID.");
+    return false;
+  }
+
+  if (!process.env.ACTIVITYTYPE) {
+    console.warn("Missing default activity type.");
+    return false;
+  }
+
+  if (!process.env.ACTIVITYMESSAGE) {
+    console.warn("Missing default activity message.");
+    return false;
+  }
+
+  if (!process.env.STATUS) {
+    console.warn("Missing default status.");
+    return false;
+  }
+
+  if (!process.env.ABOUT) {
+    console.warn("Missing about message.");
+    return false;
+  }
+
   return true;
 };
